afc3a43e06209564c2fd40eefbe58804c75936ac
oeuvars <www.anuragniall@gmail.com>
2023-07-23 07:07:56 +0530

    Stripe Fix

--- config/site.ts
+++ config/site.ts
@@ -2,10 +2,10 @@ export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
   name: "Oeushop",
-  description: "Expertly designed goods for the workspace, home, and travel.",
+  description: "A Curated Collection Of Cute and Neatly Designed Posters.",
   footer: [
     { name: "Home", href: "/" },
-    { name: "About", href: "#" },
+    { name: "About", href: 'twoxthree.online' },
     { name: "Terms & Conditions", href: "#" },
     { name: "Shipping & Return Policy", href: "#" },
     { name: "Privacy Policy", href: "#" },
